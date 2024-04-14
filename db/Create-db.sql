5fdb01f5400a2dc74545f320bb4498df4e80d871
Douglas Prado <douglas.prado@forlogic.net>
2024-04-14 00:23:48 -0300

    Criando Tabela de publicação

--- db/Create-db.sql
+++ db/Create-db.sql
@@ -2,8 +2,10 @@
 CREATE DATABASE pizzaria DEFAULT CHARACTER SET = 'utf8mb4';
 use pizzaria
 
-CREATE TABLE users (id int PRIMARY KEY AUTO_INCREMENT,name varchar(200), email varchar(200),password varchar(200),is_sys_admin int);
+CREATE TABLE users (id int PRIMARY KEY AUTO_INCREMENT,name varchar(200), email varchar(200),password varchar(200),is_sys_admin int DEFAULT 0);
 insert into users (name,email,PASSWORD,is_sys_admin )VALUES  ('Douglas','douglas.prado@forlogic.net','123',1)
+
+CREATE TABLE post (id int PRIMARY KEY AUTO_INCREMENT,name varchar(200), description varchar(200),ingredients varchar(200),value float, image LONGBLOB, id_responsible int);
 
 
 
